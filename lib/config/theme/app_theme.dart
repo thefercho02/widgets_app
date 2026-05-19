36a923c5a63957e8959af7118bed559d0756e843
ferney Pico <ferneyclash2@gmail.com>
2026-05-19 12:17:12 -0500

    feat: Update home screen

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -23,5 +23,9 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
     colorSchemeSeed: colorList[selectedColor] ,
+  appBarTheme:  const  AppBarTheme(
+    centerTitle: false,
+  )
+
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,11 +14,7 @@ class MainApp extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 4).getTheme(),
-      home:   Scaffold(
-        body: Center(
-          child: FilledButton(child: Text('guardar'), onPressed : (){}, ),
-        ),
-      ),
+      home: const  HomeScreen()
     );
   }
 }
