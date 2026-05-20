4d73eebf04dad8015302912dca6ed13159c02a20
ferney Pico <ferneyclash2@gmail.com>
2026-05-20 12:15:09 -0500

    home screen implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +7,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const  Text('Flutter + Material 3'),
-      ),
+      appBar: AppBar(title: const Text('Flutter + Material 3')),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    // ignore: unused_local_variable
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading:  Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_drop_up_rounded, color:colors.primary) ,
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap:() {
+        //TODO navegar a otra pantalla
+      } ,
     );
   }
 }
