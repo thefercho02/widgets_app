b8bcd85a3d497ac45b079f1df1f3350ee82b5c9f
ferney Pico <ferneyclash2@gmail.com>
2026-05-21 17:34:15 -0500

    configuration outes basic

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -30,26 +31,29 @@ class _HomeView extends StatelessWidget {
 }
 
 class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({
-    required this.menuItem,
-  });
+  const _CustomListTile({required this.menuItem});
 
   final MenuItem menuItem;
 
   @override
   Widget build(BuildContext context) {
-
     // ignore: unused_local_variable
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading:  Icon(menuItem.icon, color: colors.primary),
-      trailing: Icon(Icons.arrow_drop_up_rounded, color:colors.primary) ,
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_drop_up_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap:() {
-        //TODO navegar a otra pantalla
-      } ,
+      onTap: () {
+        // Navigator.of(context).push(
+        //MaterialPageRoute<void>(
+        //builder: (context) => const ButtonsScreen(),
+        //),
+        //);
+
+        Navigator.pushNamed(context, menuItem.link);
+      },
     );
   }
 }
