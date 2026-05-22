48170c0ea6126f5e4206204ddef27824df73b8d4
ferney Pico <ferneyclash2@gmail.com>
2026-05-22 12:15:57 -0500

    modifications imports my project , and implement context push

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -46,13 +47,14 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        // Navigator.of(context).push(
-        //MaterialPageRoute<void>(
-        //builder: (context) => const ButtonsScreen(),
-        //),
-        //);
+        //   Navigator.of(context).push(
+        //  MaterialPageRoute<void>(
+        //  builder: (context) => const ButtonsScreen(),
+        //  ),
+        //  );
+        //Navigator.pushNamed(context, menuItem.link);
 
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
